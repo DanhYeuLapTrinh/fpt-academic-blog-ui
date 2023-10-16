01517e149dccfc8743c6aff63fbd6ef5f27f9e27
DanhYeuLapTrinh <danhnguyen3923@gmail.com>
2023-10-17 00:00:56 +0700

    fix: img folder add to fix the distort?

--- src/user/components/molecules/Author/Author.js
+++ src/user/components/molecules/Author/Author.js
@@ -6,7 +6,7 @@ import { Stack } from '@mui/material'
 export default function Author(props) {
   return (
     <Stack direction={'row'} sx={{alignItems: 'center'}} spacing={1}>
-      <UserProfile width="28px" height="28px" src="https://images.unsplash.com/photo-1696075619158-3a70ee76b290?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2034&q=80"
+      <UserProfile width="28px" height="28px" src="../assets/img/bg.jpg"
       alt="User"/>
       <Text fontSize="12px" color={props.color}>bởi Chat GPT</Text>
       <Text fontSize="20px" color={props.color}>&middot;</Text>
