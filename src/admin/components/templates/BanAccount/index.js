58b06b1a348ec604ddd3ea86f91b304bb7dd1c45
Phuc <phucdlq@gmail.com>
2023-10-18 01:50:22 +0700

    feat: update localStorage for ban/unban account and mute/unmute account

--- src/admin/components/templates/BanAccount/index.js
+++ src/admin/components/templates/BanAccount/index.js
@@ -11,11 +11,13 @@ function BanAccountList() {
   const { auth } = useAuth();
   const [data, setData] = useState([]);
   const [records, setRecords] = useState([]);
-  const [bannedAccounts, setBannedAccounts] = useState([]);
-  const [isBanning, setIsBanning] = useState(false); // Biến để kiểm tra xem có đang trong quá trình cấm tài khoản hay không
-
+  const [isBanning, setIsBanning] = useState(false);
+  const [isBanningId, setIsBanningId] = useState(null);
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [banStatus, setBanStatus] = useState(
+    JSON.parse(localStorage.getItem("banStatus")) || {}
+  );
 
   const headers = {
     Authorization: `Bearer ${auth.token}`,
@@ -25,15 +27,12 @@ function BanAccountList() {
     axiosConfig.get("admin/users", { headers }).then((res) => {
       setData(res.data);
       setRecords(res.data);
-
-      // Tạo danh sách tài khoản bị cấm
-      const banned = res.data.filter((item) => item.isBan);
-      setBannedAccounts(banned);
-
-      console.log(res.data);
-      console.log(res.data.isBan);
     });
   }, []);
+
+  useEffect(() => {
+    localStorage.setItem("banStatus", JSON.stringify(banStatus));
+  }, [banStatus]);
 
   const handleSearch = (event) => {
     const filteredData = data.filter((item) =>
@@ -42,8 +41,17 @@ function BanAccountList() {
     setRecords(filteredData);
   };
 
-  const toggleBanStatus = (id) => {
-    setIsBanning(true); // Bắt đầu quá trình cấm tài khoản
+  const handleBanButtonClick = (id) => {
+    if (banStatus[id]) {
+      unbanAccount(id);
+    } else {
+      banAccount(id);
+    }
+  };
+
+  const banAccount = (id) => {
+    setIsBanning(true);
+    setIsBanningId(id);
     axiosConfig
       .post("admin/ban-user", { id }, { headers })
       .then((res) => {
@@ -51,25 +59,27 @@ function BanAccountList() {
           position: "top-right",
           autoClose: 3000,
         });
-        // Cập nhật lại trạng thái cấm tài khoản trong danh sách
         setRecords((prevRecords) => {
           const updatedRecords = prevRecords.map((item) => {
             if (item.id === id) {
-              return { ...item, isBan: !item.isBan };
+              return { ...item, isBan: true };
             }
             return item;
           });
           return updatedRecords;
         });
-        setIsBanning(false); // Kết thúc quá trình cấm tài khoản
+        setIsBanning(false);
+        setIsBanningId(null);
+        setBanStatus({ ...banStatus, [id]: true });
       })
       .catch((error) => {
-        toast.success("Cấm tài khoản xảy ra lỗi", {
+        toast.error("Cấm tài khoản xảy ra lỗi", {
           position: "top-right",
           autoClose: 3000,
         });
-        setIsBanning(false); // Kết thúc quá trình cấm tài khoản (trong trường hợp lỗi)
-        console.log(error);
+        setIsBanning(false);
+        setIsBanningId(null);
+        console.error(error);
       });
   };
 
@@ -81,7 +91,6 @@ function BanAccountList() {
           position: "top-right",
           autoClose: 3000,
         });
-        // Cập nhật lại danh sách tài khoản và danh sách tài khoản bị cấm
         setRecords((prevRecords) => {
           const updatedRecords = prevRecords.map((item) => {
             if (item.id === id) {
@@ -91,12 +100,12 @@ function BanAccountList() {
           });
           return updatedRecords;
         });
-        setBannedAccounts((prevBanned) =>
-          prevBanned.filter((bannedId) => bannedId !== id)
-        );
+        setBanStatus({ ...banStatus, [id]: false });
+        setIsBanning(false);
+        setIsBanningId(null);
       })
       .catch((error) => {
-        toast.success("Bỏ cấm tài khoản không thành công", {
+        toast.error("Bỏ cấm tài khoản không thành công", {
           position: "top-right",
           autoClose: 3000,
         });
@@ -139,29 +148,36 @@ function BanAccountList() {
                   <td className="p-4">{item.id}</td>
                   <td className="p-4">{item.username}</td>
                   <td className="p-4">{item.role.roleName}</td>
-                  <td className="p-4">{item.isBan ? "Cấm" : "Bình thường"}</td>
-
+                  <td className="p-4">
+                    {banStatus[item.id] ? "Bị cấm" : "Bình thường"}
+                  </td>
                   <td className="p-4 flex items-center">
-                    {isBanning ? ( // Kiểm tra nếu đang trong quá trình cấm tài khoản
+                    {banStatus[item.id] ? ( // If banStatus is true for this row, show "Bỏ cấm tài khoản" button
                       <button
-                        className="bg-blue-500 text-white px-4 py-2 rounded-lg"
-                        disabled
+                        className={`${
+                          isBanning && isBanningId === item.id
+                            ? "bg-blue-500"
+                            : "bg-green-500"
+                        } text-white px-4 py-2 rounded-lg`}
+                        onClick={() => handleBanButtonClick(item.id)}
                       >
-                        Đang cấm...
-                      </button>
-                    ) : item.isBan ? (
-                      <button
-                        className="bg-green-500 text-white px-4 py-2 rounded-lg"
-                        onClick={() => unbanAccount(item.id)}
-                      >
-                        Bỏ cấm tài khoản
+                        {isBanning && isBanningId === item.id
+                          ? "Đang cấm..."
+                          : "Bỏ cấm tài khoản"}
                       </button>
                     ) : (
+                      // If banStatus is false for this row, show "Cấm tài khoản" button
                       <button
-                        className="bg-red-500 text-white px-4 py-2 rounded-lg"
-                        onClick={() => toggleBanStatus(item.id)}
+                        className={`${
+                          isBanning && isBanningId === item.id
+                            ? "bg-blue-500"
+                            : "bg-red-500"
+                        } text-white px-4 py-2 rounded-lg`}
+                        onClick={() => handleBanButtonClick(item.id)}
                       >
-                        Cấm tài khoản
+                        {isBanning && isBanningId === item.id
+                          ? "Đang cấm..."
+                          : "Cấm tài khoản"}
                       </button>
                     )}
                   </td>
@@ -172,10 +188,10 @@ function BanAccountList() {
         <TablePagination
           rowsPerPageOptions={[5, 10, 25]}
           component="div"
-          count={records.length} // Tổng số hàng
-          page={page} // Trang hiện tại
-          onPageChange={(event, newPage) => setPage(newPage)} // Xử lý khi thay đổi trang
-          rowsPerPage={rowsPerPage} // Số hàng mỗi trang
+          count={records.length}
+          page={page}
+          onPageChange={(event, newPage) => setPage(newPage)}
+          rowsPerPage={rowsPerPage}
           onRowsPerPageChange={(event) => {
             setRowsPerPage(parseInt(event.target.value, 10));
             setPage(0);
