0ba43081b18f8201f0efaa98b05503ab07a0d25b
DanhYeuLapTrinh <danhnguyen3923@gmail.com>
2023-10-23 00:41:02 +0700

    add: character count tiptap

--- src/user/api/axios.js
+++ src/user/api/axios.js
@@ -1,7 +1,15 @@
 import axios from "axios";
 
+const baseURL = "https://api.fblog.ngrok.io/"
 export default axios.create({
-  baseURL: "https://api.fblog.ngrok.io/",
+  baseURL: baseURL,
+  headers: {
+    'Content-Type' : 'application/json',
+  },
+});
+
+export const axiosPrivate = axios.create({
+  baseURL: baseURL,
   headers: {
     'Content-Type' : 'application/json',
   },
