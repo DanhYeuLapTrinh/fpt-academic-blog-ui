2a722f7bf4515d3e18e3cc16e837721783d8e193
DanhYeuLapTrinh <danhnguyen3923@gmail.com>
2023-10-24 23:48:21 +0700

    add: tinymce lib and some of the input needed

--- src/user/components/pages/Write/Write.js
+++ src/user/components/pages/Write/Write.js
@@ -1,6 +1,9 @@
-import React from "react";
-import { Container, Stack } from "@mui/material";
+import React, { useState } from "react";
+import { Box, Button, Container, Stack } from "@mui/material";
 import PostFilter from "../../atoms/PostFilter/PostFilter";
+import ContentField from "../../organisms/ContentField/ContentField";
+import TitleField from "../../organisms/TitleField/TitleField";
+
 export default function Write() {
   return (
     <Container sx={{ p: "30px 0" }}>
@@ -10,6 +13,13 @@ export default function Write() {
         alignItems={"center"}
       >
         <PostFilter />
+      </Stack>
+      <TitleField />
+      <Box width={"100%"} height={"220px"} sx={{borderRadius: "10px",border: "3px dashed black", m:"30px 0 20px 0" }}></Box>
+      <ContentField />
+      <Stack direction={'row'} justifyContent={'flex-end'} spacing={2} paddingTop={'30px'}>
+        <Button variant="outlined">Lưu bản nháp</Button>
+        <Button variant="contained">Gửi bài</Button>
       </Stack>
     </Container>
   );
