17caef0d4599491f15698973dca736e714ac35ea
DanhYeuLapTrinh <danhnguyen3923@gmail.com>
2023-11-06 17:30:49 +0700

    add: follow accoun in profile and post

--- src/user/components/pages/ViewProfile/ViewProfileService.js
+++ src/user/components/pages/ViewProfile/ViewProfileService.js
@@ -5,15 +5,16 @@ import useAxiosPrivate from "../../../hooks/useAxiosPrivate";
 import { sortByPropertyName } from "../../../utils/StringMethod";
 import useAuth from "../../../hooks/useAuth";
 import useProfile from "../../../hooks/useProfile";
+import { async } from "q";
 
 export default function ViewProfileService() {
   const { id } = useParams();
   const profileID = Number(id);
   const axiosPrivate = useAxiosPrivate();
   const [user, setUser] = useState({});
-  const {followerList, setFollowerList} = useProfile()
-  const auth = useAuth()
-  window.scrollTo(0, 0);
+  const { followerList, setFollowerList } = useProfile();
+  const [isFollowing, setIsFollowing] = useState(false);
+  const auth = useAuth();
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -24,11 +25,20 @@ export default function ViewProfileService() {
           }
         );
         setUser(profileInfo?.data);
-        let followersList = await axiosPrivate.post(process.env.REACT_APP_VIEW_FOLLOWERS, {
-          userId: profileID,
-        })
-        setFollowerList(followersList?.data)
-        console.log(followerList)
+        let followersList = await axiosPrivate.post(
+          process.env.REACT_APP_VIEW_FOLLOWERS,
+          {
+            userId: profileID,
+          }
+        );
+
+        if (followersList) {
+          setFollowerList(followersList?.data);
+          const isFollowingUser = followersList?.data?.some(
+            (follower) => follower.id === auth.id
+          );
+          setIsFollowing(isFollowingUser);
+        }
       } catch (error) {}
     };
     fetchData();
@@ -42,9 +52,35 @@ export default function ViewProfileService() {
         process.env.REACT_APP_FOLLOW_ACCOUNT,
         {
           followedBy: auth?.id,
-          userId: id,
+          userId: profileID,
         }
       );
+      if (response) {
+        setIsFollowing(true);
+        setUser((prevUser) => ({
+          ...prevUser,
+          numOfFollower: prevUser.numOfFollower + 1,
+        }));
+      }
+    } catch (error) {}
+  };
+
+  const unfollowAccount = async () => {
+    try {
+      let response = await axiosPrivate.post(
+        process.env.REACT_APP_UNFOLLOW_ACCOUNT,
+        {
+          followedBy: auth?.id,
+          userId: profileID,
+        }
+      );
+      if (response) {
+        setIsFollowing(false);
+        setUser((prevUser) => ({
+          ...prevUser,
+          numOfFollower: prevUser.numOfFollower - 1,
+        }));
+      }
     } catch (error) {}
   };
   return (
@@ -60,6 +96,8 @@ export default function ViewProfileService() {
       userId={user?.userId}
       slug={id}
       followAccount={followAccount}
+      unfollowAccount={unfollowAccount}
+      isFollowing={isFollowing}
     />
   );
 }
