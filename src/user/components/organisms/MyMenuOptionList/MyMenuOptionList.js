d7c22603134da2cf382fb554ca1d7f42e0c63f95
DanhYeuLapTrinh <danhnguyen3923@gmail.com>
2023-11-09 14:45:40 +0700

    add: report profile

--- src/user/components/organisms/MyMenuOptionList/MyMenuOptionList.js
+++ src/user/components/organisms/MyMenuOptionList/MyMenuOptionList.js
@@ -1,8 +1,24 @@
-import {ListItemIcon, Menu, MenuItem } from "@mui/material";
+import {
+  Chip,
+  ListItemIcon,
+  Menu,
+  MenuItem,
+  Select,
+  Stack,
+} from "@mui/material";
 import { Icon } from "@iconify/react";
 import Text from "../../atoms/Text/Text";
+import { useState } from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 export default function MyMenuOptionList(props) {
+
   return (
+    <>
       <Menu
         anchorEl={props.anchorEl}
         open={props.open}
@@ -37,12 +53,59 @@ export default function MyMenuOptionList(props) {
         transformOrigin={{ horizontal: "right", vertical: "top" }}
         anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
       >
-        <MenuItem>
+        <MenuItem onClick={props.handleClickOpen}>
           <ListItemIcon>
-          <Icon icon="ic:baseline-report-gmailerrorred" color="#444746" width="22" />
+            <Icon
+              icon="ic:baseline-report-gmailerrorred"
+              color="#444746"
+              width="22"
+            />
           </ListItemIcon>
           <Text fontSize="13px">Báo cáo hồ sơ</Text>
         </MenuItem>
       </Menu>
+      <Dialog open={props.openReport} onClose={props.handleClose}>
+        <DialogTitle>Báo cáo tài khoản này?</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Nếu bạn nhận thấy có điều bất thường về tài khoản này, đừng chần chừ
+            mà hãy báo cáo ngay với chúng tôi nhé.
+          </DialogContentText>
+          <Stack
+            direction={"row"}
+            spacing={1}
+            paddingTop={2}
+            flexWrap={"wrap"}
+            justifyContent={"space-between"}
+          >
+            {props?.reportReasons?.map((item, index) => (
+              <Chip
+                label={
+                  <Text
+                    fontSize="14px"
+                    color={props.reportId === item.id ? "secondary.main" : ""}
+                  >
+                    {item.reasonName}
+                  </Text>
+                }
+                key={index}
+                onClick={() => props.setReportId(item.id)}
+                sx={{
+                  bgcolor: props.reportId === item.id ? "primary.main" : "#fff",
+                  "&:hover": {
+                    bgcolor:
+                      props.reportId === item.id ? "primary.main" : "#e0e0e0",
+                  },
+                }}
+              />
+            ))}
+          </Stack>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={props.handleCloseReport}>Hủy</Button>
+          <Button onClick={props.reportProfile}>Báo cáo</Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 }
